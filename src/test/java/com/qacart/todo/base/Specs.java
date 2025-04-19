c70bfc9c01ba0f24eff1972b4759779064e27866
Adel <muhamed.adel96@gmail.com>
2025-04-19 19:39:59 +0200

    8-Maven Command line

--- src/test/java/com/qacart/todo/base/Specs.java
+++ src/test/java/com/qacart/todo/base/Specs.java
@@ -6,9 +6,25 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
 
 public class Specs {
+    public static  String getEnv(){
+       String env = System.getProperty("env","PRODUCTION");
+       String baseURL;
+       switch (env){
+           case "PRODUCTION":
+               baseURL ="https://qacart-todo.herokuapp.com";
+           break;
+           case "LOCAL":
+               baseURL="http://localhost:8080";
+               break;
+           default:
+               throw new RuntimeException("Environment is not supported");
+       } return baseURL;
+    }
+
     public static RequestSpecification getRequestSpec(){
+
          return  given()
-                .baseUri("https://qacart-todo.herokuapp.com")
+                .baseUri(getEnv())
                 .contentType(ContentType.JSON)
                 .log().all();
     }
